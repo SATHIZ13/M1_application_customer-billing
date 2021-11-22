8f7f6dd714598a1bda41d67a0b5868b0ead9d379
SATHIZ13 <89175883+SATHIZ13@users.noreply.github.com>
2021-11-22 20:24:07 +0530

    Update customer_billing.h

--- 3_Implementation/inc/customer_billing.h
+++ 3_Implementation/inc/customer_billing.h
@@ -1,1 +1,21 @@
+/**
+ * @file Billing.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#ifndef _Billing_H
+#define _Billing_H
+
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+int qty;
+int numOfItems;
+int opt,n;
+#endif
